e96a550561fb64008d8500c1b60058daf9c3ed3e
Ahmad-J-Bary <your-email@example.com>
2026-03-14 05:59:19 +0300

    chore(core): initialize monorepo structure and fix tooling configuration errors

--- apps/desktop-mobile/vite.config.ts
+++ apps/desktop-mobile/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [react()],
   clearScreen: false,
   server: {
@@ -28,4 +28,4 @@ export default defineConfig(async () => ({
     minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
-}));
+});
